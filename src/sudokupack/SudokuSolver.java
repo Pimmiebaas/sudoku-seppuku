78e984f5bf1de718cbc6b4d0432a82c15d3f8b56
Pim aka Henk <31731356+Pimmiebaas@users.noreply.github.com>
2018-02-08 14:15:17 +0100

    nu ook met REEEEEEEsource en reader

--- src/sudokupack/SudokuSolver.java
+++ src/sudokupack/SudokuSolver.java
@@ -1,5 +1,9 @@
 package sudokupack;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class SudokuSolver {
@@ -8,29 +12,33 @@ public class SudokuSolver {
 
 		int[][] sudoku;
 		sudoku = new int[9][9];
-
-		sudoku[0][0] = 5;
-		sudoku[0][1] = 3;
-		sudoku[0][4] = 7;
-
-		sudoku[1][0] = 6;
-		sudoku[1][3] = 1;
-		sudoku[1][4] = 9;
-		sudoku[1][5] = 5;
-
-		sudoku[2][1] = 9;
-		sudoku[2][2] = 8;
-		sudoku[2][7] = 6;
-
-
+		
+		try {
+			sudoku = readFile("reeesource/sudoku1.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		SudokuSolver.toString(sudoku);
-		String[] fruitjes = new String[3];
+		String[] fruitjes = new String[4];
 		fruitjes[0] = "appel";
 		fruitjes[1] = "peer";
 		fruitjes[2] = "kiwi";
-
-		//System.out.println(fruitjes[1]);
+		fruitjes[3] = "mango";
+		
+		
+		for(int i = 0; i<fruitjes.length; i++) {
+		
+			//System.out.println(fruitjes[i]);
+		}
+		
+		int i = 0;
+		
+		while(i<fruitjes.length) {
+			System.out.println(fruitjes[i]);
+			i++;
+		}
 
 	}
 
@@ -44,13 +52,45 @@ public class SudokuSolver {
 			}
 			System.out.print("\n");
 		}
-		
-		
+
+
 	}
-	
+
 	public static int checkState(int[][] sudokuBord ) {
 		int state = 0;
 		return state;
+	}
+
+	public static int[][] readFile(String pad) throws IOException {
+
+		int[][] sudoku = new int[9][9];
+		
+		
+		
+		BufferedReader br;
+		br = new BufferedReader(new FileReader(pad));
+		String line = br.readLine();
+		int y = 0;
+		
+		while(line != null) {
+			line = line.trim(); //530090850
+			for(int x = 0; x<9; x++) {
+				
+				char pieter = line.charAt(x);
+				int fritz = Integer.valueOf(String.valueOf(pieter));
+				
+				sudoku[y][x] = fritz;
+				
+			}
+			
+			line = br.readLine();
+			
+			y++;
+		}
+
+		
+		return sudoku;
+
 	}
 
 }
